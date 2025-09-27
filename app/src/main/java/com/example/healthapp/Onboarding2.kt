e1593d2a7535481210a9dec35f33b2e8975e3b78
SandeepaChathumina <sandeepachathuminaofficial@gmail.com>
2025-09-27 10:46:01 +0530

    update onboarding 2

--- app/src/main/java/com/example/healthapp/Onboarding2.kt
+++ app/src/main/java/com/example/healthapp/Onboarding2.kt
@@ -1,6 +1,8 @@
 package com.example.healthapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +13,24 @@ class Onboarding2 : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_onboarding2)
+
+        //previous button
+
+        val previousButton: Button = findViewById(R.id.btn_previous)
+        previousButton.setOnClickListener {
+            // ✅ Navigate to OnBoarding3 instead of OnBoarding2
+            val intent = Intent(this, Onboarding1::class.java)
+            startActivity(intent)
+        }
+
+        //next button
+
+        val nextButton: Button = findViewById(R.id.btn_next)
+        nextButton.setOnClickListener {
+            // ✅ Navigate to OnBoarding3 instead of OnBoarding2
+            val intent = Intent(this, Onboarding3::class.java)
+            startActivity(intent)
+        }
 
     }
 }
