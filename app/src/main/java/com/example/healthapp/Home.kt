9124a634e86b3652593aec04a5cbd781b8b297b2
SandeepaChathumina <sandeepachathuminaofficial@gmail.com>
2025-09-27 16:50:06 +0530

    habbit/mood/hydration page polished

--- app/src/main/java/com/example/healthapp/Home.kt
+++ app/src/main/java/com/example/healthapp/Home.kt
@@ -1,8 +1,10 @@
 package com.example.healthapp
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.icu.util.Calendar
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -10,10 +12,22 @@ import androidx.cardview.widget.CardView
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class Home : AppCompatActivity() {
+    @SuppressLint("ResourceType")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_home)
+
+        setContentView(R.menu.bottom_navigation_menu)
+
+        //habit button
+
+        val habit_btn: Button = findViewById(R.id.nav_habits)
+        habit_btn.setOnClickListener {
+            // ✅ Navigate to OnBoarding3 instead of OnBoarding2
+            val intent = Intent(this, Habits::class.java)
+            startActivity(intent)
+        }
 
 
     }
